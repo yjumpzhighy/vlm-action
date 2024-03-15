0aa77619f02b7dcdf01fc0025c89e12aa1e79fd4
allen <36458664+yjumpzhighy@users.noreply.github.com>
2024-03-14 23:05:26 -0700

    Update ddpm.py

--- models/ddpm.py
+++ models/ddpm.py
@@ -5,8 +5,7 @@ import torchvision
 from torchvision import transforms
 
 from common.gaussian_diffusion import GaussianDiffusion
-from common.unet import UnetCondition
-
+from models.common.conditional_unet import ConditionalUNet
 
 if __name__ == "__main__":
     TIMESTEPS = 1000
@@ -14,57 +13,60 @@ if __name__ == "__main__":
     IMAGE_SIZE = 32
     IMAGE_C = 3
 
-
     transform = transforms.Compose([
         # transforms.Pad(4),
-        # transforms.RandomHorizontalFlip(), 
-        transforms.RandomCrop(BATCHSIZE), 
+        # transforms.RandomHorizontalFlip(),
+        transforms.RandomCrop(BATCHSIZE),
         transforms.ToTensor(),
     ])
 
     train_dataset = torchvision.datasets.CIFAR10(root='./data/cifar',
-                                                train=True, 
-                                                transform=transform,
-                                                download=False)
+                                                 train=True,
+                                                 transform=transform,
+                                                 download=False)
 
     # 测试数据集
     test_dataset = torchvision.datasets.CIFAR10(root='./data/cifar',
-                                                train=False, 
+                                                train=False,
                                                 transform=transform,
                                                 download=False)
 
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                            batch_size=BATCHSIZE, 
-                                            shuffle=True)
+                                               batch_size=BATCHSIZE,
+                                               shuffle=True)
     # 测试数据加载器
     test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                            batch_size=BATCHSIZE, 
-                                            shuffle=False)
-    
-    model = UnetCondition(IMAGE_SIZE,IMAGE_SIZE,IMAGE_C,base_channel=64,output_channel=3)
-    diffuser = GaussianDiffusion(model,IMAGE_SIZE,IMAGE_C,TIMESTEPS).cuda()   
+                                              batch_size=BATCHSIZE,
+                                              shuffle=False)
+
+    model = ConditionalUNet(IMAGE_SIZE,
+                            IMAGE_SIZE,
+                            IMAGE_C,
+                            base_channel=64,
+                            output_channel=3)
+    diffuser = GaussianDiffusion(model, IMAGE_SIZE, IMAGE_C, TIMESTEPS).cuda()
     optimizer = torch.optim.Adam(diffuser.parameters(), lr=0.001)
 
     total_step = len(train_loader)
-    num_epochs = 1 
+    num_epochs = 1
     for epoch in range(num_epochs):
         for i, (images, labels) in enumerate(train_loader):
             images = images.cuda()
-            
+
             loss = diffuser(images)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            if (i+1) % 500 == 0:
-                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
-                    .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
-    
-    with torch.no_grad():       
+            if (i + 1) % 500 == 0:
+                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(
+                    epoch + 1, num_epochs, i + 1, total_step, loss.item()))
+
+    with torch.no_grad():
         sample_batch = 1
         samples = diffuser.sample(sample_batch)
-        
+
         samples = samples.permute(0, 2, 3, 1).detach().cpu().numpy()
-        # Display images         
+        # Display images
         plt.imshow(samples[0])
         plt.axis('off')
         plt.show()
