38c19cef3f1f0f194e6ab68ac6125856863b0d77
allen <36458664+yjumpzhighy@users.noreply.github.com>
2024-03-28 00:28:59 -0700

    add fiskr dataset in ddpm.py

--- models/ddpm.py
+++ models/ddpm.py
@@ -7,68 +7,59 @@ import os
 from common.gaussian_diffusion import GaussianDiffusion
 from common.conditional_unet import ConditionalUNet
 from common.utils import inception_score, frechet_inception_distance
+from common.datasets import FlickrDataset, MnistDataset, CifarDataset
+
 
 if __name__ == "__main__":
-    
     TIMESTEPS = 1000
-    BATCHSIZE = 128
-    IMAGE_SIZE = 32
+    BATCHSIZE = 16
+    IMAGE_SIZE = 200
     IMAGE_C = 3
-    NUM_EPOCHS = 0
+    NUM_EPOCHS = 100
     SAVE_MODEL_PATH = 'data/ddpm/best.pth'
-
-    transform = transforms.Compose([
-        # transforms.Pad(4),
-        # transforms.RandomHorizontalFlip(),
-        #transforms.RandomCrop(IMAGE_SIZE),
-        transforms.ToTensor(),
-    ])
-
-    train_dataset = torchvision.datasets.CIFAR10(root='./data/cifar',
-                                                 train=True,
-                                                 transform=transform,
-                                                 download=False)
-
-    # 测试数据集
-    test_dataset = torchvision.datasets.CIFAR10(root='./data/cifar',
-                                                train=False,
-                                                transform=transform,
-                                                download=False)
-
-    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                               batch_size=BATCHSIZE,
-                                               shuffle=True)
-    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                              batch_size=BATCHSIZE,
-                                              shuffle=False)
+    # data_train = CifarDataset('/home/zuyuanyang/Projects/vlm/data/cifar', IMAGE_SIZE, 
+    #                           transforms.Compose([transforms.ToTensor()]), 'train')
+ 
+    from transformers import DistilBertTokenizer
+    tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
+    data_train = FlickrDataset("/home/zuyuanyang/Data/flickr30k/captions_fixed.csv",
+                     "/home/zuyuanyang/Data/flickr30k/flickr30k_images/",
+                     tokenizer,
+                     256,
+                     IMAGE_SIZE)
+    train_loader = torch.utils.data.DataLoader(
+        data_train,
+        batch_size=BATCHSIZE,
+        num_workers=4,
+        shuffle=True)
 
     diffuser = GaussianDiffusion(ConditionalUNet(IMAGE_SIZE,
                                                 IMAGE_SIZE,
                                                 IMAGE_C,
                                                 base_channel=64,
-                                                output_channel=3),
+                                                output_channel=IMAGE_C),
                                  IMAGE_SIZE, IMAGE_C, TIMESTEPS, 'pred_noise').cuda()
     optimizer = torch.optim.Adam(diffuser.parameters(), lr=0.001)
 
     total_step = len(train_loader)
     cur_loss = np.finfo(np.float32).max
     for epoch in range(NUM_EPOCHS):
-        for i, (images, labels) in enumerate(train_loader):
-            images = images.cuda()
-
-            mask = (labels == 5)
-            images = images[mask]
-            if images.shape[0]==0:
-                continue
-
-
-            loss = diffuser(images)
+        for idx, batch in enumerate(train_loader):
+            x = batch['image']
+            if 'label' in batch.keys():
+                mask = (batch['label'] == 5)
+                x = x[mask]
+                if x.shape[0]==0:
+                    continue
+            x = x.cuda()
+                
+            loss = diffuser(x)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
         print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(
-                    epoch + 1, NUM_EPOCHS, i + 1, total_step, loss.item()))
+                    epoch + 1, NUM_EPOCHS, idx + 1, total_step, loss.item()))
 
         # save best model
         if loss.item() < cur_loss:
@@ -99,3 +90,8 @@ if __name__ == "__main__":
         # samples = samples.view(-1, 3)
         # print(inception_score(samples))
         # print(frechet_inception_distance(samples, samples))
+        
+    
+        
+        
+        
