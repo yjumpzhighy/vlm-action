4f793989adc8670ba6c86715b318994646f3a578
allen <36458664+yjumpzhighy@users.noreply.github.com>
2024-08-29 23:02:23 -0700

    Update vae.py

--- models/vae.py
+++ models/vae.py
@@ -1,97 +1,225 @@
 import torch
-from torch import nn
-import torch.nn.functional as F
-from torch import optim
-
-import matplotlib.pyplot as plt
-import os
 import numpy as np
-from scipy.stats import norm
-from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import torchvision
 from torchvision import transforms
-from torchvision.datasets import MNIST
+import torch.distributed as dist
+import os
+from torch.cuda.amp import autocast, GradScaler
 
-from common.variational_auto_encoder import VariationalAutoEncoder
-from common.datasets import FlickrDataset, MnistDataset, CifarDataset
+from common.variational_auto_encoder import VariationalAutoEncoder, MyAutoencoderKL
+from common.conditional_unet import ConditionalUNet
+from common.utils import ddp_load_checkpoint, cosine_with_warmup_scheduler, init_setup
+from common.datasets import ImageNetDataset, ButterfliesDataset
+
 
 if __name__ == "__main__":
-    EPOCHS = 100
-    BATCH_SIZE = 512
-    IMAGE_SIZE = 32
+    init_setup(seed=12345)
+    dist.init_process_group(backend='nccl')
+    torch.cuda.set_device(dist.get_rank())
+
+    BATCHSIZE = 16
+    IMAGE_SIZE = 256
     IMAGE_C = 3
-    EMBEDDING_DIM = 64
+    NUM_EPOCHS = 500
+    EMBEDDING_BASE_DIM = 128
+    EMBEDDING_MULTIS = (1,2,4,4)
     LATENT_DIM = 4
+    LEARNING_RATE = 1e-5
+    SAVE_MODEL_PATH = 'data/vae/vae_butterfly256_bs16_latent4_f8_embed128_ep500.pth'
+    TRAIN_VAE = True
 
     from transformers import CLIPTokenizer
     tokenizer = CLIPTokenizer.from_pretrained('openai/clip-vit-base-patch32')
-    data_train = CifarDataset(
-        os.path.abspath(os.path.join(os.getcwd(), '../../Data/cifar10')),
+    # data_train = ImageNetDataset(
+    #         os.path.abspath(os.path.join(os.getcwd(), '../../Data/imagenet')),
+    #         IMAGE_SIZE,
+    #         transforms.Compose([
+    #             transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+    #             transforms.ToTensor(),
+    #             transforms.Normalize([0.5], [0.5])
+    #         ]), 'train[0:50000]', tokenizer, 64)
+    
+    data_train = ButterfliesDataset(
+        os.path.abspath(os.path.join(os.getcwd(), '../../Data/butterflies')),
         IMAGE_SIZE,
         transforms.Compose([
             transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
-            transforms.ToTensor()
-        ]), 'train', tokenizer)
+            transforms.ToTensor(),
+            transforms.Normalize([0.5], [0.5])  #transform to [-1,1]
+        ]),
+        'train', tokenizer, 64)
+
+    data_sampler = torch.utils.data.distributed.DistributedSampler(
+            data_train,
+            num_replicas=dist.get_world_size(),
+            rank=dist.get_rank(),
+            shuffle=True)
 
     train_loader = torch.utils.data.DataLoader(data_train,
-                                               batch_size=BATCH_SIZE,
-                                               num_workers=4,
-                                               shuffle=True)
+                                                   batch_size=BATCHSIZE,
+                                                   sampler=data_sampler,
+                                                   num_workers=12,
+                                                   pin_memory=True)
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = VariationalAutoEncoder(IMAGE_SIZE,
+    unet = ConditionalUNet(IMAGE_SIZE,
+                            IMAGE_SIZE,
+                            IMAGE_C,
+                            base_channel=EMBEDDING_BASE_DIM,
+                            channel_mults=EMBEDDING_MULTIS,
+                            output_channel=IMAGE_C,
+                            add_down_up_skip=False).cuda()
+
+    model = VariationalAutoEncoder(unet,
+                                   IMAGE_SIZE,
                                    IMAGE_C,
-                                   EMBEDDING_DIM,
                                    output_chann=IMAGE_C,
-                                   latent_chann=LATENT_DIM).to(device)
-    optimizer = model.configure_optimziers(lr=1e-4)[0]
+                                   latent_chann=LATENT_DIM,
+                                   use_GAN=True).cuda()
 
-    for epoch in range(EPOCHS):
-        model.train()
-        train_num = len(train_loader.dataset)
-        for idx, batch in enumerate(train_loader):
-            x = batch['image'].to(device)
+    # model = MyAutoencoderKL().cuda()
 
-            *_, loss, log = model(x)
+                            
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        print(f"Training loss {loss: .3f} in Epoch {epoch}")
-    torch.save(model.state_dict(), 'data/vae/best.pth')
+    if TRAIN_VAE:
+        ddp_model = torch.nn.parallel.DistributedDataParallel(
+            model,
+            device_ids=[dist.get_rank()],
+            find_unused_parameters=True)
 
-    n = 3
-    linspace = 10
-    grids = [
-        norm.ppf(np.linspace(0.05, 0.95, linspace))
-        for i in range(model.encoded_image_h * model.encoded_image_w *
-                       LATENT_DIM)
-    ]
+        encoder_optimizer = ddp_model.module.configure_optimziers(lr=LEARNING_RATE)[0]
+        discriminator_optimizer = ddp_model.module.configure_optimziers(
+            lr=LEARNING_RATE)[1]
 
-    figure = np.zeros((IMAGE_SIZE * n, IMAGE_SIZE * n, IMAGE_C))
-    with torch.no_grad():
-        for i in range(n * n):
-            z_sampled = np.array([
-                np.random.choice(grids[i])
-                for i in range(model.encoded_image_h * model.encoded_image_w *
-                               LATENT_DIM)
-            ]).reshape(1, LATENT_DIM, model.encoded_image_h,
-                       model.encoded_image_w)
-            z_sampled = torch.FloatTensor(z_sampled).cuda()
-            decode = model(z_sampled, mode='decode')[0]
-            digit = decode[0].permute(1, 2,
-                                      0).contiguous().detach().cpu().numpy()
+        from diffusers.optimization import get_cosine_schedule_with_warmup
+        encoder_lr_scheduler = get_cosine_schedule_with_warmup(
+            optimizer=encoder_optimizer,
+            num_warmup_steps=50,
+            num_training_steps=(len(train_loader) * NUM_EPOCHS),
+        )
 
-            fig_row_idx = int(i / n)
-            fig_col_idx = int(i % n)
-            figure[fig_row_idx * IMAGE_SIZE:(fig_row_idx + 1) * IMAGE_SIZE,
-                   fig_col_idx * IMAGE_SIZE:(fig_col_idx + 1) *
-                   IMAGE_SIZE, :] = digit
+        if discriminator_optimizer is not None:
+            discriminator_lr_scheduler = get_cosine_schedule_with_warmup(
+                optimizer=discriminator_optimizer,
+                num_warmup_steps=50,
+                num_training_steps=(len(train_loader) * NUM_EPOCHS),
+            )
+        else:
+            discriminator_lr_scheduler = None
 
-    figure = (figure * 255).astype('uint8')
-    if IMAGE_C == 1:
-        figure = np.squeeze(figure, axis=-1)
-        plt.imsave('vae_image.png', figure, cmap='gray', format='png')
-    elif IMAGE_C == 3:
-        plt.imsave('vae_image.png', figure, format='png')
-    else:
-        raise ValueError("image channels not supported.")
+        encoder_loss = []
+        discriminator_loss = []
+        encoder_scaler = GradScaler()
+        discriminator_scaler = GradScaler()
+        min_loss = np.finfo(np.float32).max
+        optimizer_idx = 0
+        global_step = 0
+        for epoch in range(NUM_EPOCHS):
+            ddp_model.train()
+            for idx, batch in enumerate(train_loader):
+                x = batch['image']
+                x = x.cuda()
+
+                with autocast():
+                    loss = ddp_model(
+                        x,
+                        optimizer_idx=optimizer_idx,
+                        gan_factor=1.0 if (epoch >= int(NUM_EPOCHS*0.8)) else 0.0,
+                        global_step=global_step)
+                    main_loss = loss[0]
+
+                if optimizer_idx == 0:
+                    scheduler = encoder_lr_scheduler
+                    optimizer = encoder_optimizer
+                    scaler = encoder_scaler
+                    encoder_loss = loss
+                else:
+                    scheduler = discriminator_lr_scheduler
+                    optimizer = discriminator_optimizer
+                    scaler = discriminator_scaler
+                    discriminator_loss = loss
+
+                optimizer.zero_grad()
+                scaler.scale(main_loss).backward()
+                scaler.step(optimizer)
+                scaler.update()
+                scheduler.step()
+
+                if discriminator_optimizer is not None:
+                    optimizer_idx = (optimizer_idx + 1) % 2
+
+                global_step += dist.get_world_size()
+
+            if dist.get_rank() == 0:
+                print(epoch,
+                    "/", 
+                    NUM_EPOCHS,
+                    ": autoencoder loss",
+                    [val.item() for val in encoder_loss],
+                    " /// discriminator loss",
+                    [val.item() for val in discriminator_loss],
+                )
+
+                if encoder_loss[0].item() < min_loss:
+                    min_loss = encoder_loss[0].item()
+                    torch.save(ddp_model.state_dict(), SAVE_MODEL_PATH)
+    
+    dist.barrier()
+    state_dict = ddp_load_checkpoint(SAVE_MODEL_PATH, dist.get_rank())
+    model.load_state_dict(state_dict, strict=True)
+
+
+    if dist.get_rank() == 0:
+        with torch.no_grad():
+
+            data_iter = iter(train_loader)
+            batch = next(data_iter)
+            figure = np.zeros((IMAGE_SIZE, IMAGE_SIZE, IMAGE_C))
+            figure[:,:,:] = batch['image'][0].permute(1, 2,
+                                            0).contiguous().detach().cpu().numpy()
+            figure = (figure * 255).astype('uint8')
+            plt.imsave('vae_raw_image.png', figure, format='png')
+
+            img = batch['image'].cuda()
+            z_sampled = model(img, mode='encode').sample()
+
+            img_reconstruct = model(z_sampled, mode='decode')
+            figure = np.zeros((IMAGE_SIZE, IMAGE_SIZE, IMAGE_C))
+            figure[:,:,:] = img_reconstruct[0].permute(1, 2,
+                                            0).contiguous().detach().cpu().numpy()
+            figure = (figure * 255).astype('uint8')
+            plt.imsave('vae_reconstruct_image.png', figure, format='png')
+
+    if dist.get_rank() == 0:
+        with torch.no_grad():
+            # z_sampled = torch.randn(
+            #     (9, LATENT_DIM, model.encoded_image_h,
+            #            model.encoded_image_w)).cuda()
+            z_sampled = torch.randn(
+                (9, 4, 32, 32)).cuda()
+            samples = model(z_sampled, mode='decode')
+            samples = samples.permute(
+                0, 2, 3, 1).contiguous().detach().cpu().numpy()  #[b,h,w,c]
+
+            figure = np.zeros((IMAGE_SIZE * 3, IMAGE_SIZE * 3, IMAGE_C))
+            for i in range(9):
+                fig_row_idx = int(i / 3)
+                fig_col_idx = int(i % 3)
+                figure[fig_row_idx * IMAGE_SIZE:(fig_row_idx + 1) * IMAGE_SIZE,
+                    fig_col_idx * IMAGE_SIZE:(fig_col_idx + 1) *
+                    IMAGE_SIZE, :] = samples[i]
+
+            figure = (figure * 255).astype('uint8')
+            if IMAGE_C == 1:
+                figure = np.squeeze(figure, axis=-1)
+                plt.imsave('vae_image.png', figure, cmap='gray', format='png')
+            elif IMAGE_C == 3:
+                plt.imsave('vae_image.png', figure, format='png')
+            else:
+                raise ValueError("image channels not supported.")
+
+
+
+            
+            
+
